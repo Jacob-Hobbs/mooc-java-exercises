678ad94c3507d10ec86665cf6f4d71c66b38cbd3
Jacob-Hobbs <75915516+Jacob-Hobbs@users.noreply.github.com>
2023-08-01 12:52:03 -0500

    Add files via upload

---
+++ part-12-solutions/part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -0,0 +1,28 @@
+
+public class Program {
+
+    public static void main(String[] args) {
+        int rows = 2;
+        int columns = 3;
+        int[][] matrix = new int[rows][columns];
+        matrix[0][1] = 5;
+        matrix[1][0] = 3;
+        matrix[1][2] = 7;
+        System.out.println(arrayAsString(matrix));
+        
+    }
+    
+    public static String arrayAsString(int[][] array) {
+        StringBuilder numbers = new StringBuilder();
+        
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                String value = String.valueOf(array[row][column]);
+                numbers.append(value);
+            }
+            numbers.append("\n");
+        }
+        return numbers.toString();
+    }
+
+}
