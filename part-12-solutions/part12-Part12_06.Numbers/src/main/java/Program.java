2c6423859d753620a8ce9b83c19d802ab153898d
Jacob-Hobbs <75915516+Jacob-Hobbs@users.noreply.github.com>
2023-08-01 11:52:17 -0500

    Add files via upload

---
+++ part-12-solutions/part12-Part12_06.Numbers/src/main/java/Program.java
@@ -0,0 +1,21 @@
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Program {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        Random random = new Random();
+        
+        System.out.println("How many random numbers should be printed?");
+        int num = Integer.valueOf(scanner.nextLine());
+        
+        for (int i = 1; i <= num; i++) {  
+            int randomNumber = random.nextInt(11);
+            System.out.println(randomNumber);    
+        }
+    }
+
+}
